ecf2b2a7f68aac3a6e7b8437467492ef8b550f69
vrnsky <vrnsky@protonmail.ch>
2023-10-07 18:22:22 +0800

    Initial stub implementation

---
+++ src/main/java/my/edu/sunway/wbrms/wbrmsreportservice/WbrmsReportServiceApplication.java
@@ -0,0 +1,13 @@
+package my.edu.sunway.wbrms.wbrmsreportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbrmsReportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbrmsReportServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/edu/sunway/wbrms/wbrmsreportservice/WbrmsReportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package my.edu.sunway.wbrms.wbrmsreportservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbrmsReportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
